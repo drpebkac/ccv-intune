64fdb389327098e19fcac43732941dc53f5a7188
Doctor Pebkac <67497646+drpebkac@users.noreply.github.com>
2021-05-04 15:32:26 +1000

    Added error handing

--- ccv-psscripts/PSEnable-LinuxSubSys.ps1
+++ ccv-psscripts/PSEnable-LinuxSubSys.ps1
@@ -1,1 +1,21 @@
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
+if((Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux).State -eq "Enabled")
+{
+  Write-Host "Windows Linux Subsystem already enabled"
+  Write-Host "No action taken"
+  exit
+  
+}
+else
+{
+  try
+  {
+    Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
+    
+  }
+  catch [Exception]
+  {
+    Write-Host "Linux Subsystem will not be installed for unknown reasons.
+    
+  }
+   
+  
